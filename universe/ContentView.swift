60d92ef7871e882eff5a439d7e592c5ba3c91542
Adiom Timur <adiom@canfly.org>
2025-01-09 21:09:29 +0400

    Initial Commit

---
+++ universe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  universe
+//
+//  Created by adiom on 09.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ universe/universeApp.swift
@@ -0,0 +1,17 @@
+//
+//  universeApp.swift
+//  universe
+//
+//  Created by adiom on 09.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct universeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
